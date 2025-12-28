029c44d53c6988305063b48e0b36729e23ed434d
mmd1375-pir <mohammadpirahmadi75@gmail.com>
2025-12-28 13:44:13 +0330

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 int main() {
     cout << "Hello from my Git test!" << endl;
+    for(int i =0; i <= 3; i++)
+        cout<<rand()<<endl;
+    
     return 0;
 }
